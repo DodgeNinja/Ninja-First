3ee88b166958841851d41e3b4f67a0dc5f628f8a
2007sehyun <jungsehyun3669@gmail.com>
2023-06-01 20:30:06 +0900

    버튼 활용
    라이트 활용

---
+++ Assets/02_Scripts/Sehyun/NinJaProjectNum2/ButtonCube.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonCube : MonoBehaviour
+{
+    public Transform door;
+    private void OnMouseUpAsButton()
+    {
+        
+        
+        Debug.Log("The Cube Button Works");
+    }
+
+    void WallMove()
+    {
+       
+    }
+}
