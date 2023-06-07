4e4120d3be99dd5835cb83650d68b049f13f1351
JangWheesung <gameplay1qaz@gmail.com>
2023-06-07 20:07:26 +0900

    Collection

--- Assets/02_Scripts/Jang/CollectManager.cs
+++ Assets/02_Scripts/Jang/CollectManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public enum Collection
@@ -13,13 +14,33 @@ public class CollectManager : MonoBehaviour
 {
     public static CollectManager instance;
 
-    float coll1;
-    float coll2;
-    float coll3;
+    [Header("Text")]
+    [SerializeField] private TextMeshProUGUI coll1Text;
+    [SerializeField] private TextMeshProUGUI coll2Text;
+    [SerializeField] private TextMeshProUGUI coll3Text;
+    [Header("Value")]
+    [SerializeField] private int collect1Max;
+    [SerializeField] private int collect2Max;
+    [SerializeField] private int collect3Max;
+    [SerializeField] private int collectLength;
+    private int coll1, coll2, coll3, maxLength;
 
     private void Awake()
     {
         instance = this;
+    }
+
+    private void Update()
+    {
+        AllCollect();
+    }
+
+    private void AllCollect()
+    {
+        if (maxLength >= collectLength)
+        {
+            //엔딩스크립트 enable 켜주기
+        }
     }
 
     public void OnColl(Collection col)
@@ -28,12 +49,21 @@ public class CollectManager : MonoBehaviour
         {
             case Collection.collect1:
                 coll1++;
+                coll1Text.text = $"Collect1 {coll1}/{collect1Max}";
+                if (coll1 >= collect1Max)
+                    maxLength++;
                 break;
             case Collection.collect2:
                 coll2++;
+                coll2Text.text = $"Collect2 {coll2}/{collect2Max}";
+                if (coll2 >= collect2Max)
+                    maxLength++;
                 break;
             case Collection.collect3:
                 coll3++;
+                coll3Text.text = $"Collect3 {coll3}/{collect3Max}";
+                if (coll3 >= collect3Max)
+                    maxLength++;
                 break;
         }
     }
