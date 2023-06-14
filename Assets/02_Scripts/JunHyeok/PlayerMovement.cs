81aa887d123d506c6d613a2facb051bf2833f205
JangWheesung <gameplay1qaz@gmail.com>
2023-06-14 18:25:51 +0900

    메인 게임 씬 수정

--- Assets/02_Scripts/JunHyeok/PlayerMovement.cs
+++ Assets/02_Scripts/JunHyeok/PlayerMovement.cs
@@ -79,6 +79,7 @@ public class PlayerMovement : MonoBehaviour
 
             rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
             rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            Debug.Log(rotationX);
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
 
