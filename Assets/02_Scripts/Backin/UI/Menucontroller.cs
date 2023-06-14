fceed530942b712c58e48ce751bfa9b39c6903a7
dlstjd0237 <a49918074@gmail.com>
2023-06-14 18:24:59 +0900

    2023_06_14

--- Assets/02_Scripts/Backin/UI/Menucontroller.cs
+++ Assets/02_Scripts/Backin/UI/Menucontroller.cs
@@ -3,34 +3,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using DG.Tweening;
 
 
 public class Menucontroller : MonoBehaviour
 {
     private UIDocument _doc;
-    private Button _playButton;
-    private Button _settingsButton;
-    private Button _ExitButton;
-    private Button _MuteButton;
+
+    private Button _playButton; //플레이버튼
+    private Button _settingsButton; //세팅버튼
+    private Button _ExitButton; // 나가기 버튼
+    private Button _MuteButton; // 조용히 버튼
+    private Button _ExitNoButton; // 나가기 안에 있는 No라는 선택지 버튼
+
+    private VisualElement _quit;//바텀시트의 부모
+    private VisualElement _quitSheet; //Quit화면 위로 올라가기
     [SerializeField]private UnityEngine.UI.Image _image;
+
+  
+
     private void Awake()
     {
-        _doc = GetComponent<UIDocument>();
-        _playButton = _doc.rootVisualElement.Q<Button>("PlayButton");
+        var _doc = GetComponent<UIDocument>();
+        #region 플레이 버튼
+        _playButton = _doc.rootVisualElement.Q<Button>("PlayButton"); //플레이 버튼
         _playButton.clicked += PlayButtonOnClicked;
-
+        #endregion
         _settingsButton = _doc.rootVisualElement.Q<Button>("SettingsButton");
-
+        #region 게임 종료 버튼
         _ExitButton = _doc.rootVisualElement.Q<Button>("ExitButton");
         _ExitButton.clicked += ExitButtonOnClicked;
-
+        #endregion
         _MuteButton = _doc.rootVisualElement.Q<Button>("MuteButton");
 
+        _quit = _doc.rootVisualElement.Q<VisualElement>("Quit-Bottom");
+        _quit.style.display = DisplayStyle.None;
+
+        _ExitNoButton = _doc.rootVisualElement.Q<Button>("NoButton");
+        _ExitNoButton.clicked += ExitButton_No;
+
+        _quitSheet = _doc.rootVisualElement.Q<VisualElement>("QuitSheet");
     }
 
     private void ExitButtonOnClicked()
     {
-        Application.Quit();
+        _quit.style.display = DisplayStyle.Flex;
+
+
     }
 
     private void PlayButtonOnClicked()
@@ -38,6 +57,11 @@ public class Menucontroller : MonoBehaviour
         StartCoroutine(PlayEvent());
     }
 
+    private void ExitButton_No()
+    {
+        _quit.style.display = DisplayStyle.None;
+
+    }
     IEnumerator PlayEvent()
     {
         while (true)
