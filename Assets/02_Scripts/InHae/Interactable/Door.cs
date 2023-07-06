ec22d650af7965f0feaa7beaf153a984aa0a17d0
inhae0519 <lsh266409@gmail.com>
2023-07-07 00:22:19 +0900

    수동문 완성

--- Assets/02_Scripts/InHae/Interactable/Door.cs
+++ Assets/02_Scripts/InHae/Interactable/Door.cs
@@ -8,17 +8,31 @@ public class Door : Interactable
     [SerializeField] int speed;
 
     private GameObject player;
+    [SerializeField]
+    private Animator animator;
+    public bool isOpen = false;
+    public bool isBackOpen = false;
+    private Door[] doors = new Door[3];
 
     private void Awake()
     {
+        doors = GetComponentsInChildren<Door>();
         player = GameObject.FindWithTag("Player");
     }
 
     protected override void Interact()
     {
         Debug.Log("Interactable with " + gameObject.name);
-        Reder();
+
+        Debug.Log(isOpen);
+        if (isOpen)
+        {
+            Debug.Log("Close");
+            Close();
+        }
+        else Reder();
     }
+
 
     void Reder()
     {
@@ -33,21 +47,62 @@ public class Door : Interactable
         }
     }
 
-    void FrontRader()
+    void Close()
     {
-        if (transform.localScale.x > 0)
+        Vector3 vec = player.transform.position - transform.position;
+        if (Mathf.Acos(Vector3.Dot(vec.normalized, transform.forward)) * Mathf.Rad2Deg > 90)
         {
-            transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * speed);
+            Debug.Log("ff");
+            FrontClose();
         }
-
-        if (transform.localScale.x < 0)
+        else
         {
-            transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * speed);
+            Debug.Log("dd");
+            BackClose();
         }
     }
 
+    void FrontRader()
+    {
+        animator.SetBool("open", true);
+    }
+
+    void FrontClose()
+    {
+        animator.SetBool("open", false);
+    }
+
     void BackRader()
-    { 
-        
+    {
+        animator.SetBool("backOpen", true);
+    }
+
+    void BackClose()
+    {
+        animator.SetBool("backOpen", false);
+    }
+
+    public void OpenSet()
+    {
+        foreach (Door door in doors) door.isOpen = true;
+        foreach (Door door in doors) door.isBackOpen = true;
+    }
+
+    public void BackOpenSet()
+    {
+        foreach (Door door in doors) door.isBackOpen = true;
+        foreach (Door door in doors) door.isOpen = true;
+    }
+
+    public void CloseSet()
+    {
+        foreach (Door door in doors) door.isOpen = false;
+        foreach (Door door in doors) door.isBackOpen = false;
+    }
+
+    public void BackCloseSet()
+    {
+        foreach (Door door in doors) door.isBackOpen = false;
+        foreach (Door door in doors) door.isOpen = false;
     }
 }
